d4747123dd260ab43718c8e1d4c13e8894c41be4
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2025-12-14 21:17:16 -0300

    Add header/footer templates and update site layout
    Introduced reusable header and footer JavaScript templates. Replaced inline SVG and header markup in index.html with new templates. Added DCO and FC logo images. Updated HTML and CSS files to integrate the new structure and improve layout consistency.

---
+++ templates/header.js
@@ -0,0 +1,25 @@
+class AppHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <div class="header-container">
+            <h1 class="fheader">ACEUs - Departamento de Computação</h1>
+
+            <div class = "logo-dco">
+                <svg width="280" height="113" viewBox="0 0 280 113" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                <rect width="280" height="113" rx="47" fill="url(#pattern0_228_27)"/>
+                <defs>
+                <pattern id="pattern0_228_27" patternContentUnits="objectBoundingBox" width="1" height="1">
+                <use xlink:href="#image0_228_27" transform="matrix(0.00115194 0 0 0.00287019 -0.135396 -0.537002)"/>
+                </pattern>
+                <image id="image0_228_27" width="1024" height="768" preserveAspectRatio="none" href="images/dco_logo.png"/>
+                </defs>
+                </svg>
+            </div>
+        </div>
+    </header>
+    `;
+  }
+}
+
+customElements.define('app-header', AppHeader);
